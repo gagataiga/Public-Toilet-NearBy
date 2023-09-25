cea23c5d6963eda75c04a823264be6afbb2363c1
TaigaShiga <gaitatg97@gmail.com>
2023-09-25 14:05:02 +0900

    feat: add NODE_ENV

--- db/index.ts
+++ db/index.ts
@@ -1,4 +1,5 @@
 const knex = require("knex");
 const knexConf = require("../knexfile");
+const env:string = process.env.NODE_ENV || "development";
 
-module.exports = knex(knexConf[""])
+module.exports = knex(knexConf[env]);
