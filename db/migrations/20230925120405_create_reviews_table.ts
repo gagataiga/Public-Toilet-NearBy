100a2ec6d5534429da72b3ccdd8acc35d2f668b5
TaigaShiga <gaitatg97@gmail.com>
2023-09-25 21:16:01 +0900

    feat: fix post_id to posts_id

--- db/migrations/20230925120405_create_reviews_table.ts
+++ db/migrations/20230925120405_create_reviews_table.ts
@@ -1,6 +1,5 @@
 import { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
   return await knex.schema.createTable("reviews", (table) => {
     table.increments("review_id").primary();
@@ -10,11 +9,10 @@ export async function up(knex: Knex): Promise<void> {
     table.integer("user_id").unsigned();
     table.foreign("user_id").references("user_id").inTable("users").onDelete("CASCADE");
     table.integer("post_id").unsigned();
-    table.foreign("post_id").references("post_id").inTable("posts").onDelete("CASCADE");
+    table.foreign("post_id").references("posts_id").inTable("posts").onDelete("CASCADE");
     table.integer("rating");
   });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
   return await knex.schema.dropTable("reviews");
