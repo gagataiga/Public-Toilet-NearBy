e742fa406083285eda44cacfc8353cdb0c9db592
TaigaShiga <gaitatg97@gmail.com>
2023-09-25 21:10:40 +0900

    feat: add create_reviews_table

---
+++ db/migrations/20230925120405_create_reviews_table.ts
@@ -0,0 +1,22 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  return await knex.schema.createTable("reviews", (table) => {
+    table.increments("review_id").primary();
+    table.text("comment").notNullable();
+    table.timestamp("created_at");
+    table.timestamp("updated_at");
+    table.integer("user_id").unsigned();
+    table.foreign("user_id").references("user_id").inTable("users").onDelete("CASCADE");
+    table.integer("post_id").unsigned();
+    table.foreign("post_id").references("post_id").inTable("posts").onDelete("CASCADE");
+    table.integer("rating");
+  });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  return await knex.schema.dropTable("reviews");
+}
+
