970f39b07e7e2edfcf9cd1d958798e460cc65c73
TaigaShiga <gaitatg97@gmail.com>
2023-09-27 02:41:41 +0900

    feat: add css for navigation bar to be shown from the right side

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useRef , useState} from 'react'
 import "./Header.css";
+
 function Header() {
+  const navRef = useRef();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggelNavBar = () => { 
+    setIsOpen(!isOpen);
+  }
+
   return (
     <header className='header'>
       <div className='header__item_title'>
           <h2>Near-Me-Toilet</h2>
       </div>
-      <nav className='header__item_nav'>
-        <ul className='nav__items'>
-          <li>About</li>
-          <li>Sign in / Sign up</li>
-          <li>Settings</li>
-          <li>Log out</li>
-        </ul>
+      <nav className='header__nav_menu'>
+        <div className="nav__button_container">
+          <button className='nav__button' onClick={toggelNavBar}>MENU</button>
+        </div>
+      
+        <div className={`nav__items_container ${isOpen ? "open" : ""}`}>
+          <ul className='nav__items'>
+            <li className='nav__item'><a href="#">About</a></li>
+            <li className='nav__item'><a href="#">Login</a></li>
+            <li className='nav__item'><a href="#">Setting</a></li>
+            <li className='nav__item'><a href="#">Posts</a> </li>
+            <li className='nav__item'><a href="#">Logout</a> </li>
+          </ul>
+        </div>
       </nav>
     </header>
   )
