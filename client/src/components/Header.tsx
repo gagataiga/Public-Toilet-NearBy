7ad8255fd674c3e07b04a731ce0dc27ec075029a
TaigaShiga <gaitatg97@gmail.com>
2023-09-27 15:36:20 +0900

    feat: force navbar to close when title or link is pressed

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -9,10 +9,12 @@ function Header() {
     setIsOpen(!isOpen);
   }
 
+  const closedNavBar = () => {setIsOpen(false)}
+
   return (
     <header className='header'>
       <div className='header__item_title'>
-          <Link to="/"><h2 className='heaer__item_link'>Near-Me-Toilet</h2></Link>
+          <Link to="/"><h2 className='heaer__item_link' onClick={closedNavBar}>Near-Me-Toilet</h2></Link>
       </div>
       <nav className='header__nav_menu'>
         <div className="nav__button_container">
@@ -21,12 +23,12 @@ function Header() {
       
         <div className={`nav__items_container ${isOpen ? "open" : ""}`}>
           <ul className='nav__items'>
-          <li className='nav__item'><Link to="/" className='nav__item_link'>Home</Link></li>
-            <li className='nav__item'><Link to="/about" className='nav__item_link'>About</Link></li>
-            <li className='nav__item'><Link to="/auth" className='nav__item_link'>Login</Link></li>
-            <li className='nav__item'><Link to="/setting" className='nav__item_link'>Setting</Link></li>
-            <li className='nav__item'><Link to="/posts" className='nav__item_link'>Posts</Link> </li>
-            <li className='nav__item'><Link to="/logout" className='nav__item_link'>Logout</Link> </li>
+          <li className='nav__item'><Link to="/" className='nav__item_link' onClick={toggelNavBar}>Home</Link></li>
+            <li className='nav__item'><Link to="/about" className='nav__item_link' onClick={toggelNavBar}>About</Link></li>
+            <li className='nav__item'><Link to="/auth" className='nav__item_link' onClick={toggelNavBar}>Login</Link></li>
+            <li className='nav__item'><Link to="/setting" className='nav__item_link' onClick={toggelNavBar}>Setting</Link></li>
+            <li className='nav__item'><Link to="/posts" className='nav__item_link' onClick={toggelNavBar}>Posts</Link> </li>
+            <li className='nav__item'><Link to="/logout" className='nav__item_link' onClick={toggelNavBar}>Logout</Link> </li>
           </ul>
         </div>
       </nav>
