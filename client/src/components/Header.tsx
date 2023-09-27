3e6d2efb0b890737c4d7831c33aae46422f21d10
TaigaShiga <gaitatg97@gmail.com>
2023-09-28 01:08:43 +0900

    feat: generate each link using map

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,6 +2,9 @@ import React, { useState} from 'react'
 import "./Header.css";
 import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from 'react-router-dom';
+import { navLinks } from '../common/data/navLinks';
+import { NavLink } from '../common/types';
+
 function Header() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
@@ -23,12 +26,11 @@ function Header() {
       
         <div className={`nav__items_container ${isOpen ? "open" : ""}`}>
           <ul className='nav__items'>
-          <li className='nav__item'><Link to="/" className='nav__item_link' onClick={toggelNavBar}>Home</Link></li>
-            <li className='nav__item'><Link to="/about" className='nav__item_link' onClick={toggelNavBar}>About</Link></li>
-            <li className='nav__item'><Link to="/auth" className='nav__item_link' onClick={toggelNavBar}>Login</Link></li>
-            <li className='nav__item'><Link to="/setting" className='nav__item_link' onClick={toggelNavBar}>Setting</Link></li>
-            <li className='nav__item'><Link to="/posts" className='nav__item_link' onClick={toggelNavBar}>Posts</Link> </li>
-            <li className='nav__item'><Link to="/logout" className='nav__item_link' onClick={toggelNavBar}>Logout</Link> </li>
+            {navLinks.map((link: NavLink, index: number) => {
+              return (
+                <li className='nav__item' key={index}><Link to={link.to} className='nav__item_link' onClick={toggelNavBar}>{link.label}</Link></li>
+              )
+            })}
           </ul>
         </div>
       </nav>
