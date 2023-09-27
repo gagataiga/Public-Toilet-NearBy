7de427d014071dee055bbd264f8892ef35c5b58c
TaigaShiga <gaitatg97@gmail.com>
2023-09-27 11:17:26 +0900

    feat: add Auth page and AuthForm component.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import Landing from './pages/Landing';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Route, Routes } from 'react-router-dom';
+import Auth from './pages/Auth';
 
 function App() {
  
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path='/landing' element={<Landing />} />
         <Route path='/' element={<Home />} />
+        <Route path='/auth' element={<Auth/>} />
       </Routes>
     <Footer />
     </div>
