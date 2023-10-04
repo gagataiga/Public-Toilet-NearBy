267fe90db093f4aa53262e1e7aa41ce9b2bdbfb8
TaigaShiga <gaitatg97@gmail.com>
2023-10-04 17:09:21 +0900

    feat: get user current lcoation

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { LatLng } from "leaflet";
 import LocationMaker from './LocationMarker';
 const Map = () => {
   const position = new LatLng(51.505, -0.09); 
   
+  useEffect(() => { 
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+         console.log(position);
+        },
+        (error) => {
+          console.error("Error getting user location: ", error);
+        }
+      );
+    } else {
+      console.log("Geolocation is not supported by your browser");
+    }
+  }, []); 
+
   return (
     <div>
       <MapContainer center={position} zoom={14} style={{height: "70vh", width: "100vw"}}>
