1eecede56549aa25cae5a7b6439b3ed06ef880d1
TaigaShiga <gaitatg97@gmail.com>
2023-10-09 16:38:11 +0900

    feat: upload user location

--- server/model/locations-model.ts
+++ server/model/locations-model.ts
@@ -1,11 +1,11 @@
 const locationKnex = require("../../db/index");
-const LOCATIONS_TABLE: string = "Locations";
+const LOCATIONS_TABLE: string = "locations";
 
-module.exports = {
+module.exports = { 
   LOCATIONS_TABLE,
-  async insertLocation(locagion:{longitude: number, latitude: number}) {
+  async insertLocation(location:{longitude: number, latitude: number}) {
     try {
-      const [registerLocationId] = await locationKnex(LOCATIONS_TABLE).insert(locagion).returning("location_id");
+      const [registerLocationId] = await locationKnex(LOCATIONS_TABLE).insert(location).returning("location_id");
       return registerLocationId;
     } catch (error) {
       throw error;
