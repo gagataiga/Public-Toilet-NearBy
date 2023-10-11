1ec4349a3c974d05b7af8a4f1b075eaa852a79c8
TaigaShiga <gaitatg97@gmail.com>
2023-10-11 15:15:52 +0900

    feat: show routes to the toilet the user wants to go to

---
+++ client/src/api/locationService.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+const apiUrl: string = process.env.REACT_APP_API_URL || "";
+
+export const getNavigation = async (start: string, end: string) => {
+  console.log("get navigation");
+  try {
+    const result = await axios.get(`${apiUrl}locations/navigation`, {
+      params: {   
+        start: start,
+        end: end
+      }
+    });
+    return result.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
--- client/src/components/PostMarker.tsx
+++ client/src/components/PostMarker.tsx
@@ -1,41 +1,63 @@
 import React, { useEffect, useState } from 'react'
 import { getAllPosts } from '../api/postService';
-import { Marker, Popup } from "react-leaflet";
-import L from "leaflet";
-import "leaflet-routing-machine";
-import 'leaflet-routing-machine/dist/leaflet-routing-machine.css'
+import { Marker, Polyline, Popup } from "react-leaflet";
+import { getNavigation } from '../api/locationService';
+import { useAppSelector } from '../redux/hooks';
+import { Location } from '../common/types';
+import { Button } from '@mui/material';
 
 const PostMaker = () => {
   const [posts, setPosts] = useState([]);
-  
+  const [destination, setDestination] = useState<number[]>([]);
+  const locationState: Location = useAppSelector((state) => state.location);
+  const [routes, setRoutes] = useState<[][]>([]);
+
   const fetchAllPosts = async () => { 
     const response = await getAllPosts();
     setPosts(response);
   }
-
- 
-  const handleClick = async () => { 
-    console.log("L",);
+  
+  const handleClick = async (lat:number, lng:number) => { 
+    const start: string = `${locationState.lng},${locationState.lat}`;
+    const end:string = `${lng},${lat}`;
+    const result = await getNavigation(start, end);
+    console.log(result);
+    const routesArray = result.features[0].geometry.coordinates;
+    console.log(routesArray);
+    const reversedRoutes = routesArray.map((route:any) => [route[1],route[0]]);
+    console.log(reversedRoutes);
+    
+    setRoutes(reversedRoutes);
+    setDestination([lng, lat]);
   }
 
   useEffect(() => {
     fetchAllPosts();
   }, []);
 
-  console.log("posts", posts);
-
   return (
     <div>
-      {posts.map((post:any, index) => { 
-        return (
-        <Marker key={index} position={{lng:post.longitude, lat:post.latitude}} >
-          <Popup>
-              <button onClick={handleClick}>Do you wanna go there ?</button>
+      {destination.length > 0 ? (<>
+        <Marker  position={{ lng: destination[0], lat: destination[1] }} >
+            <Popup>
+              your destination
           </Popup>
         </Marker>
-        );
-      })
-      }
+        <Polyline positions={routes} color="blue" />
+      </>) : (
+      <>
+            {posts.map((post: any, index) => {
+              return (
+                <Marker key={index} position={{ lng: post.longitude, lat: post.latitude }} >
+                  <Popup>
+                    
+                    <Button onClick={() => handleClick(post.latitude, post.longitude)}>Do you want to go there ?</Button>
+                  </Popup>
+                </Marker>
+              );
+            })}
+      </>
+      )}
     </div>
   )
 }
