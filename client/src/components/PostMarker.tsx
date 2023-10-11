83e3faa5d9a86f8a408c6bd294623784aa3f10a0
TaigaShiga <gaitatg97@gmail.com>
2023-10-11 13:39:27 +0900

    feat: get routing infomation from /locations/navigation endpoint

--- client/src/components/PostMarker.tsx
+++ client/src/components/PostMarker.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { getAllPosts } from '../api/postService';
 import { Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import "leaflet-routing-machine";
+import 'leaflet-routing-machine/dist/leaflet-routing-machine.css'
 
 const PostMaker = () => {
-
   const [posts, setPosts] = useState([]);
   
   const fetchAllPosts = async () => { 
@@ -11,6 +13,10 @@ const PostMaker = () => {
     setPosts(response);
   }
 
+ 
+  const handleClick = async () => { 
+    console.log("L",);
+  }
 
   useEffect(() => {
     fetchAllPosts();
@@ -24,9 +30,7 @@ const PostMaker = () => {
         return (
         <Marker key={index} position={{lng:post.longitude, lat:post.latitude}} >
           <Popup>
-          <div style={{height: "auto", width: "auto", fontSize:"30px",overflow:  "auto"}}>
-          A pretty CSS3 popup. Easily customizable
-          </div>
+              <button onClick={handleClick}>Do you wanna go there ?</button>
           </Popup>
         </Marker>
         );
--- server/router/locations-router.ts
+++ server/router/locations-router.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, Response, request } from "express";
 
 const express = require("express");
 const router = express.Router();
@@ -14,5 +14,21 @@ router.post("/",async (req:Request, res:Response) => {
     res.status(400).send("bad request");
   }
 })
+
+router.get("/navigation", async (req: Request, res: Response) => {
+  try { 
+    const { start, end } = req.query;
+    const key = process.env.OPEN_ROUTE_API_KEY;
+    console.log(key);
+    console.log(start, end);
+    const URL = `https://api.openrouteservice.org/v2/directions/foot-walking?api_key=${key}&start=${start}&end=${end}`
+    const result = await fetch(URL);
+    const data = await result.json();
+    res.status(200).send(data);
+  }catch(error){
+    console.log(error);
+    res.status(400).send("bad request");
+  }
+});
 
 module.exports = router;
