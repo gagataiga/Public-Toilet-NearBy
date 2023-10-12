ee642c701da651192bc0a6ba860b50137048756e
TaigaShiga <gaitatg97@gmail.com>
2023-10-13 01:25:26 +0900

    feat: change default location

--- client/src/components/LocationMarker.tsx
+++ client/src/components/LocationMarker.tsx
@@ -8,17 +8,15 @@ const LocationMaker = () => {
   const radius: number = 30;
   return (
     <div>
-      // eslint-disable-next-line
-      //@ts-ignore  
-      <Circle center={(locationState.lng && locationState.lat) ? { lng: locationState.lng, lat: locationState.lat } : [51.505, -0.09]} radius={radius}>
-         <Marker position={(locationState.lng && locationState.lat) ? { lng: locationState.lng, lat: locationState.lat} : [51.505, -0.09]} >
+      {/* <Circle center={(locationState.lng && locationState.lat) ? { lng: locationState.lng, lat: locationState.lat } : [51.505, -0.09]} > */}
+        <Marker position={(locationState.lng && locationState.lat) ? { lng: locationState.lng, lat: locationState.lat} : [51.505, -0.09]} >
         <Popup >
           <div style={{height: "100", width: "auto", fontSize:"30px",overflow:  "auto"}}>
           Your location
           </div>
         </Popup>
       </Marker>
-      </Circle>
+      {/* </Circle> */}
     </div>
   )
 }
