323b92561c807f3d92b41908e5e513d0b8a7b73f
TaigaShiga <gaitatg97@gmail.com>
2023-10-13 00:03:47 +0900

    feat: center should be array

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -46,7 +46,7 @@ const Map = (props: MapProps) => {
             <div className='post-map_container skeleton-loader'></div>
           ) : (
           <div className='post-map'>
-            <MapContainer className='post-map_container' center={(locationState.lng && locationState.lat) ? { lng: locationState.lng, lat: locationState.lat } : [51.505, -0.09]} zoom={15}>
+            <MapContainer className='post-map_container' center={(locationState.lng && locationState.lat) ? [locationState.lng, locationState.lat ] : [51.505, -0.09]} zoom={15}>
              <TileLayer
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
