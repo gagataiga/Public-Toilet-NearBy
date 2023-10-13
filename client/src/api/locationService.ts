45db0143a07a70ac4dacb891aa42a169a8133854
TaigaShiga <gaitatg97@gmail.com>
2023-10-14 01:13:11 +0900

    feat: fetch data from openrouteservice

--- client/src/api/locationService.ts
+++ client/src/api/locationService.ts
@@ -3,13 +3,24 @@ const apiUrl: string = process.env.REACT_APP_API_URL || "";
 
 export const getNavigation = async (start: string, end: string) => {
   try {
-    const result = await axios.get(`${apiUrl}/locations/navigation`, {
+    const URL = `https://api.openrouteservice.org/v2/directions/foot-walking`
+    const key = process.env.OPEN_ROUTE_API_KEY;
+    const data = await axios.get(URL, {
       params: {
+        api_key: key,
         start: start,
         end: end
       }
-    });
-    return result.data;
+    }); 
+    console.log(data);
+  // try {
+  //   const result = await axios.get(`${apiUrl}/locations/navigation`, {
+  //     params: {
+  //       start: start,
+  //       end: end
+  //     }
+  //   });
+  //   return result.data;
   } catch (error) {
     console.error(error);
   }
