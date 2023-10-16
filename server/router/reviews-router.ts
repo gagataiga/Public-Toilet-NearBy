9c35917ebbd96270f325548dee105b04a1ac6e6f
TaigaShiga <gaitatg97@gmail.com>
2023-10-16 09:46:55 +0900

    feat: make insertReview

--- server/router/reviews-router.ts
+++ server/router/reviews-router.ts
@@ -7,12 +7,20 @@ const reviewsModel = require("../model/reviews-model");
 router.get("/:postId", async (req: Request, res: Response) => { 
   try {
     const postId = req.params.postId;
-    
     const result = await reviewsModel.getReviews(Number(postId));
-    
     res.status(200).send(result);
   } catch (error) {
     console.error(error);
+    res.status(400).send("bad request");
+  }
+})
+
+router.post("/",async (req:Request, res:Response) => {
+  try {
+    const review = req.body;
+    console.log();
+  } catch (error) {
+    console.log(error);
     res.status(400).send("bad request");
   }
 })
