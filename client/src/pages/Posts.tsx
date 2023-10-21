c0ced2d4d03c3048f10ca6cb04be87251ba82a1c
TaigaShiga <gaitatg97@gmail.com>
2023-10-22 00:51:56 +0900

    feat: delete user-posts test data

--- client/src/pages/Posts.tsx
+++ client/src/pages/Posts.tsx
@@ -23,54 +23,12 @@ const Posts = () => {
     fetchPosts();
   }, []);
 
-  const postData = [
-    {
-      comment: "This is nice",
-      cost: "Free",
-      facilities: [0, 1],
-      image_url: "https://firebasestorage.googleapis.com/v0/b/near-me-toilet.appspot.com/o/posts%2Fbathroom-5944194_640.jpg?alt=media&token=14dedddb-7742-4bd3-8278-cc68a84f8dc5",
-      latitude: 36.042585,
-      longitude: 139.712448,
-      post_id: 2,
-      rating: 5
-    },
-    {
-      comment: "This is nice",
-      cost: "Free",
-      facilities: [0, 1],
-      image_url: "https://firebasestorage.googleapis.com/v0/b/near-me-toilet.appspot.com/o/posts%2Fbathroom-5944194_640.jpg?alt=media&token=14dedddb-7742-4bd3-8278-cc68a84f8dc5",
-      latitude: 36.042585,
-      longitude: 139.712448,
-      post_id: 2,
-      rating: 5
-    },
-    {
-      comment: "This is nice",
-      cost: "Free",
-      facilities: [0, 1],
-      image_url: "https://firebasestorage.googleapis.com/v0/b/near-me-toilet.appspot.com/o/posts%2Fbathroom-5944194_640.jpg?alt=media&token=14dedddb-7742-4bd3-8278-cc68a84f8dc5",
-      latitude: 36.042585,
-      longitude: 139.712448,
-      post_id: 2,
-      rating: 5
-    },
-    {
-      comment: "This is nice",
-      cost: "Free",
-      facilities: [0, 1],
-      image_url: "https://firebasestorage.googleapis.com/v0/b/near-me-toilet.appspot.com/o/posts%2Fbathroom-5944194_640.jpg?alt=media&token=14dedddb-7742-4bd3-8278-cc68a84f8dc5",
-      latitude: 36.042585,
-      longitude: 139.712448,
-      post_id: 2,
-      rating: 5
-    }
-  ];
   
   return (
     <div className='own-posts_container'>
     <h3 className='post-title'>Your Posts</h3>
     <div className='own-posts'>
-        {postData.map((post, index) => { 
+        {userPosts.map((post, index) => { 
         return (
           <div className='own-post' key={index}>
             <UserPost post={post} isPostsPage={true} />
